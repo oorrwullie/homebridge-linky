35c6d7a06f976e6126dcf9f69eb1dea9fdf43ef7
Kevin Payne <payneks@gmail.com>
2025-04-27 10:02:18 -0600

    fix(tsconfig): output ESM modules correctly for Node.js v20 runtime 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { API } from 'homebridge';
 import { LinkyPlatform } from './linkyPlatform';
 
-export = (api: API) => {
+export default (api: API) => {
   api.registerPlatform('homebridge-linky', 'Linky', LinkyPlatform);
 };
