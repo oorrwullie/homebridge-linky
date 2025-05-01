4eb0addab88326f28fcfc48477a6e32c5446bdbe
Kevin Payne <payneks@gmail.com>
2025-05-01 11:04:38 -0600

    chore: fix import 🚀

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { API } from 'homebridge';
 import { LinkyPlatform } from './linkyPlatform.js';
 
 export default (api: API) => {
-  api.registerPlatform('homebridge-linky', 'Linky', LinkyPlatform);
+  api.registerPlatform('Linky', LinkyPlatform);
 };
