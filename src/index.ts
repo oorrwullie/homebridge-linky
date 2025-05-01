58d885193f6be668de0ee5251f4f8effa1cb6ff0
Kevin Payne <payneks@gmail.com>
2025-05-01 10:26:59 -0600

    fix: restore CommonJS export in index for Homebridge compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { API } from 'homebridge';
-import { LinkyPlatform } from './linkyPlatform';
+import { LinkyPlatform } from './linkyPlatform.js';
 
 export default (api: API) => {
   api.registerPlatform('homebridge-linky', 'Linky', LinkyPlatform);
